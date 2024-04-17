3b08ea710069a7d1077be5aa7337fbfb7dce06f3
momoka.sugiura <momoka.sugiura.d088@dirbato.co.jp>
2024-04-17 10:53:52 +0900

    Gitの練習です

---
+++ techacademy_jdbc/src/dbSample/util/DatabaseManager.java
@@ -0,0 +1,34 @@
+package dbSample.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+	//データベース接続と結果取得のための変数
+	private static Connection con;
+	
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+		// 1. ドライバのクラスをJava上で読み込む
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		// 2.DBと接続する
+		con = DriverManager.getConnection(
+			"jdbc:mysql://localhost/world?useSSL=false&allowPublicKeyRetrieval=true",
+			"root"
+			"Sumomo-412"
+		);
+		
+		return con;
+	}
+
+	public static void close() {
+		// 7. 接続を閉じる
+		if(con != null) {
+			try {
+				con.close();
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
